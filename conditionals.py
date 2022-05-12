765f38167ef209229ff48f758728bd0099930350
Praveen Dias <praveendias1180@gmail.com>
2022-05-12 21:55:15 +0530

    Conditional Python

---
+++ conditionals.py
@@ -0,0 +1,10 @@
+time = 13
+
+if time < 12:
+    print ("It's not 12 yet!")
+    print ("Good Morning!")
+if time > 12:
+    print("It's past 12!")
+    print("Good afternoon!")
+
+print("Have a good day!")
