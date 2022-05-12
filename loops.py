4f53c75452420d8907d51a8cdbda38fe01341f46
Praveen Dias <praveendias1180@gmail.com>
2022-05-13 00:08:55 +0530

    while loop

--- loops.py
+++ loops.py
@@ -10,3 +10,16 @@ print('--------------------------------')
 
 for item in range(2,20, 3):
     print(item)
+
+
+print('--------------------------------')
+
+a=0
+for i in range(2,4):
+    a=a+i
+print(a)
+
+num = int(input("Enter your number: "))
+while (num != 0):
+    print("Hello Praveen")
+    num -= 1
