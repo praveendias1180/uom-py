1ffbcbfb31d25bba7b385ea071c8fe986fc553ae
Praveen Dias <praveendias1180@gmail.com>
2022-05-13 08:48:43 +0530

    File writing

--- files.py
+++ files.py
@@ -1,4 +1,9 @@
 fhandle = open("myfile.txt")
-fcontents = fhandle.read()
+fcontents = fhandle.readline()
 print(fcontents)
+fhandle.close()
+
+fhandle = open("myfile.txt", "w")
+mystring = "This is how \n files are written"
+fhandle.write(mystring)
 fhandle.close()
