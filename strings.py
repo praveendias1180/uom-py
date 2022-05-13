4ebb214d5ec24804f77cd5cf4210330c4ee87258
Praveen Dias <praveendias1180@gmail.com>
2022-05-13 09:29:06 +0530

    Strings

--- strings.py
+++ strings.py
@@ -6,3 +6,11 @@ print(word[3:6])
 print(word[-1])
 
 print(word[-4:])
+
+print(len(word))
+
+print('a' in word)
+
+print("My name is %s and I am %d years old!" % ('Praveen', 20))
+
+print(str(float(4)))
