685533c6d001ceaf7b9f7b38fe25d3dae88ec069
Praveen Dias <praveendias1180@gmail.com>
2022-05-13 06:30:52 +0530

    Print formatting

--- input.py
+++ input.py
@@ -4,3 +4,9 @@
 print(1,2,3,4)
 print(1,2,3,4, sep="-")
 print(1,2,3,4, sep="-", end="#")
+print("")
+
+a = 1; b = 2;
+print("The value of a is {} and the value of b is {}".format(1,2))
+print("The value of a is {} and the value of b is {}".format(a, b))
+print("The value of a is {2} and the value of b is {1}".format(11,22,33))
