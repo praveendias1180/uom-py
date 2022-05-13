a640c7dfc418eb0d82e362e37c58a05ed1fa47ab
Praveen Dias <praveendias1180@gmail.com>
2022-05-13 07:04:08 +0530

    Math lib and _doc_

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+import math
 y = max(1,2,3,4,5,6,7)
 
 def printme(str):
@@ -12,6 +13,13 @@ def greet(name):
     print("Hello, " + name)
 
 greet("Nimal")
+
+def circum(r):
+    "Calculate circumference and return"
+    return 2* math.pi *r
+
+print(circum(2))
+print(circum.__doc__)
 
 
 print(y)
